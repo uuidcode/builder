e26af75d1fafb60f5260e3fa3660a2fbe01552ce
uuidcode <uuidcode@gmail.com>
2019-03-14 16:00:22 +0900

    wait

--- src/main/java/com/github/uuidcode/builder/selenium/ChromeDriverBuilder.java
+++ src/main/java/com/github/uuidcode/builder/selenium/ChromeDriverBuilder.java
@@ -2,9 +2,9 @@ package com.github.uuidcode.builder.selenium;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,6 +15,7 @@ import org.openqa.selenium.support.ui.Wait;
 import org.slf4j.Logger;
 
 import com.github.uuidcode.util.CoreUtil;
+import com.github.uuidcode.util.StringStream;
 
 import static com.github.uuidcode.util.CoreUtil.base64Decode;
 import static com.github.uuidcode.util.CoreUtil.splitListWithColon;
@@ -28,6 +29,12 @@ public class ChromeDriverBuilder {
     private ChromeDriver driver;
     private String host;
     private int sleepSecond = 2;
+    private int scrollTopOffset = 200;
+
+    public ChromeDriverBuilder setScrollTopOffset(int scrollTopOffset) {
+        this.scrollTopOffset = scrollTopOffset;
+        return this;
+    }
 
     public int getSleepSecond() {
         return this.sleepSecond;
@@ -70,29 +77,44 @@ public class ChromeDriverBuilder {
         this(new ChromeOptions());
     }
 
-    public ChromeDriverBuilder click(WebElement webElement) {
-        try {
-            this.wait(webElement).click();
-        } catch (Throwable t) {
-            if (logger.isErrorEnabled()) {
-                logger.error(">>> error ChromeDriverBuilder click", t);
-            }
-        }
-
-        return this.sleep();
-    }
-
     private Wait getWait() {
         return new FluentWait<>(driver)
                 .withTimeout(Duration.ofSeconds(5))
                 .pollingEvery(Duration.ofSeconds(1));
     }
 
-    private WebElement wait(WebElement webElement) {
+    public ChromeDriverBuilder scrollDownByClassName(String className) {
+        String method = "document.getElementsByClassName('selector').scrollTop"
+            .replaceAll("selector", className);
+        return this.scroll(method);
+    }
+
+    public ChromeDriverBuilder scrollDownById(String id) {
+        String method = "document.getElementById('selector').scrollTop"
+            .replaceAll("selector", id);
+        return this.scroll(method);
+    }
+
+    public ChromeDriverBuilder scroll(int value) {
+        return this.scroll(String.valueOf(value));
+    }
+
+    public ChromeDriverBuilder scroll(String value) {
+        String script = StringStream.of()
+            .add("var scrollTop = expression;".replaceAll("expression", value))
+            .add("window.scrollTo(0, scrollTop);")
+            .joiningWithSpace();
+
+        driver.executeScript(script);
+        return this.sleep();
+    }
+
+    private WebElement wait(By by) {
         Wait wait = this.getWait();
-        wait.until(ExpectedConditions.visibilityOfAllElements(webElement));
-        driver.executeScript("arguments[0].scrollIntoView()", webElement);
-        return webElement;
+        wait.until(ExpectedConditions.presenceOfElementLocated(by));
+        WebElement element = this.driver.findElement(by);
+        this.scroll(element.getLocation().y - this.scrollTopOffset);
+        return element;
     }
 
     public ChromeDriverBuilder loadUrl(String url) {
@@ -120,42 +142,21 @@ public class ChromeDriverBuilder {
         return this;
     }
 
-    public ChromeDriverBuilder click(Function<String, WebElement> function, String selector) {
+    public ChromeDriverBuilder click(By by) {
         try {
-            this.click(function.apply(selector));
+            this.wait(by).click();
         } catch (Throwable t) {
             if (logger.isErrorEnabled()) {
-                logger.error(">>> error clickByClassName", t);
+                logger.error(">>> error ChromeDriverBuilder click", t);
             }
         }
 
         return this;
     }
 
-    public ChromeDriverBuilder click(String selector) {
-        return click(this.driver::findElementByCssSelector, selector);
-    }
-
-    public ChromeDriverBuilder sendKey(Function<String, WebElement> function,
-                                       String selector, String value) {
+    public ChromeDriverBuilder sendKey(By by, String value) {
         try {
-            this.sendKey(function.apply(selector), value);
-        } catch (Throwable t) {
-            if (logger.isErrorEnabled()) {
-                logger.error(">>> error clickByClassName", t);
-            }
-        }
-
-        return this;
-    }
-
-    public ChromeDriverBuilder sendKey(String selector, String value) {
-        return this.sendKey(this.driver::findElementByCssSelector, selector, value);
-    }
-
-    public ChromeDriverBuilder sendKey(WebElement webElement, String value) {
-        try {
-            this.wait(webElement);
+            WebElement webElement = this.wait(by);
             webElement.clear();
             webElement.sendKeys(value);
         } catch (Throwable t) {
@@ -217,20 +218,9 @@ public class ChromeDriverBuilder {
         List<String> valueList = splitListWithColon(base64Decode(loginForm.getToken()));
 
         this.loadUrl(loginForm.getUri());
-        this.sendKey(loginForm.getFirst(), valueList.get(0));
-        this.sendKey(loginForm.getSecond(), valueList.get(1));
+        this.sendKey(By.cssSelector(loginForm.getFirst()), valueList.get(0));
+        this.sendKey(By.cssSelector(loginForm.getSecond()), valueList.get(1));
 
-        return this.click(loginForm.getThird());
-    }
-
-    public ChromeDriverBuilder click(String selector, String innerText) {
-        this.driver
-            .findElementsByCssSelector(selector)
-            .stream()
-            .filter(element -> element.getAttribute("innerText").equals(innerText))
-            .findFirst()
-            .ifPresent(this::click);
-
-        return this.sleep();
+        return this.click(By.cssSelector(loginForm.getThird()));
     }
 }
